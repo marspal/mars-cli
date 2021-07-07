0b320f7f920f442d5d7836f230ab7de9015ef483
xu andy <andyxu@xus-MacBook-Pro.local>
2021-07-07 22:56:21 +0800

    feat: init 命令的准备阶段

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Command = require("@mars-cli/command");
 const log = require("@mars-cli/log");
 
@@ -11,7 +13,34 @@ class InitCommand extends Command {
         log.verbose("force: ", this.force);
     }
     exec() {
+        try {
+            // 1. 准备阶段
+            this.prepare();
+            // 2. 下载模板
+            // 3. 安装模板
+        } catch (e) {
+            log.error(e.message);
+        }  
+    }
 
+    prepare(){
+        // 1. 当前目录是否为空
+        if(!this.isCwdEmpty()) {
+            // 1.1询问是否继续创建
+        }
+        console.log(ret);
+        // 2. 是否启动强制更新
+        // 3. 选择项目或者组件
+        // 4. 获取项目的基本信息
+    }
+
+    isCwdEmpty() {
+        const localPath = process.cwd();
+        let fileList = fs.readdirSync(localPath);
+        fileList = fileList.filter((file) => (
+            !file.startsWith(".") && ['node_modules'].includes(file)
+        ));
+        return !fileList || fileList.length <= 0;
     }
 }
 
