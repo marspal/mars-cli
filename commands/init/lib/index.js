cdbe00a134e7e960956ef7f5872f6f934ba3e347
xu andy <andyxu@xus-MacBook-Pro.local>
2021-07-12 00:37:56 +0800

    feat: 项目名称,版本号校验

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -2,8 +2,14 @@
 
 const fs = require('fs');
 const path = require('path');
+const inrequirer = require('inquirer');
+const fse = require('fs-extra');
+const semver = require('semver');
 const Command = require("@mars-cli/command");
 const log = require("@mars-cli/log");
+
+const TYPE_PROJECT = "project";
+const TYPE_COMPONENT = "component";
 
 class InitCommand extends Command {
     init() {
@@ -12,10 +18,10 @@ class InitCommand extends Command {
         log.verbose("projectName: ", this.projectName);
         log.verbose("force: ", this.force);
     }
-    exec() {
+    async exec() {
         try {
             // 1. 准备阶段
-            this.prepare();
+            await this.prepare();
             // 2. 下载模板
             // 3. 安装模板
         } catch (e) {
@@ -23,22 +29,122 @@ class InitCommand extends Command {
         }  
     }
 
-    prepare(){
+    async prepare(){
+        const localPath = process.cwd();
         // 1. 当前目录是否为空
-        if(!this.isCwdEmpty()) {
+        if(!this.isDirEmpty(localPath)) {
             // 1.1询问是否继续创建
+            let ifContinue = false;
+            if(!this.force) {
+                ifContinue = (await inrequirer.prompt({
+                    type: "confirm",
+                    name: "ifContinue",
+                    message: "当前项目不为空, 是否继续创建?",
+                    default: false
+                })).ifContinue;
+                if(!ifContinue) {
+                    return;
+                }
+            }
+
+            if (ifContinue || this.force) {
+                // 2. 是否启动强制更新: 清空目录
+                const {confirmDelete} = await inrequirer.prompt({
+                    type: "confirm",
+                    name: "confirmDelete",
+                    message: "是否清空当前目录下的文件",
+                    default: false
+                });
+                if (confirmDelete) {
+                    // 清空目录
+                    fse.emptyDirSync(localPath);
+                }
+            }
         }
-        console.log(ret);
-        // 2. 是否启动强制更新
-        // 3. 选择项目或者组件
-        // 4. 获取项目的基本信息
+        
+        // return 项目的基本信息 object
+        return this.getProjectInfo();
     }
 
-    isCwdEmpty() {
-        const localPath = process.cwd();
+    async getProjectInfo() {
+        const projectInfo = {};
+        // 3. 选择项目或者组件
+        const {type} = await inrequirer.prompt({
+            type: 'list',
+            name: 'type',
+            message: '请选择初始化类型',
+            default: TYPE_PROJECT,
+            choices: [
+                {
+                    name: '项目',
+                    value: TYPE_PROJECT,
+                },
+                {
+                    name: '组件',
+                    value: TYPE_COMPONENT,
+                },
+            ]
+        });
+        log.verbose('初始化类型type: ',  type);
+        if (type === TYPE_PROJECT) {
+            // 4. 获取项目的基本信息
+            const o = await inrequirer.prompt([{
+                type: "input",
+                name: "projectName",
+                message: "请输入项目名称",
+                default: "",
+                validate: function(v){
+                    // 首字符必须是字母
+                    // 尾字符必须为字母和数字、不能为字符
+                    // 字符仅允许-_
+                    var done = this.async();
+                    setTimeout(function() {
+                        if (!/^[a-zA-Z]([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)) {
+                            done('请输入正确的项目名称。');
+                            return;
+                        }
+                        done(null, true);
+                    }, 0);
+                },
+                filter: function(v){
+                    return v;
+                }
+            }, {
+                type: "input",
+                name: "projectVersion",
+                message: "请输入项目版本号",
+                default: "1.0.0",
+                validate: function(v){
+                    // 版本号校验
+                    var done = this.async();
+                    setTimeout(function() {
+                        if (!(!!semver.valid(v))) {
+                            // Pass the return value in the done callback
+                            done('请输入合法的版本号。');
+                            return;
+                        }
+                        done(null, true);
+                    }, 0);
+                },
+                filter: function(v){
+                    if (!!(semver.valid(v))) {
+                        return semver.valid(v);
+                    } else {
+                        return v;
+                    }
+                }
+            }]);
+            console.log(o);
+        } else if(type === TYPE_COMPONENT) {
+
+        }
+        return projectInfo;
+    }
+
+    isDirEmpty(localPath) {
         let fileList = fs.readdirSync(localPath);
         fileList = fileList.filter((file) => (
-            !file.startsWith(".") && ['node_modules'].includes(file)
+            !file.startsWith(".") && !['node_modules'].includes(file)
         ));
         return !fileList || fileList.length <= 0;
     }
